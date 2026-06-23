b167d9e418981ab6993c381da8e5ffb82dafecf0
Andre2646 <andre.lima25@aluno.cps.sp.gov.br>
2026-06-23 12:53:03 -0300

    Implementação do saldo inicial e menu

--- caixa_eletronico.c
+++ caixa_eletronico.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Caixa Eletronico\n");
+
+    float saldo = 1000.00;
+    int opcao;
+
+    printf("1 - Verificar saldo\n");
+    printf("7 - Sair\n");
+
+    scanf("%d", &opcao);
+
+    if(opcao == 1) {
+        printf("Saldo: %.2f\n", saldo);
+    }
+
     return 0;
 }
