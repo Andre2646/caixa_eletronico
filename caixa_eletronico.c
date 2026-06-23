15531d14ab0a19663a66e608a75e4a64b55df551
Andre2646 <andre.lima25@aluno.cps.sp.gov.br>
2026-06-23 12:55:41 -0300

    Finalização do caixa eletrônico

--- caixa_eletronico.c
+++ caixa_eletronico.c
@@ -3,37 +3,88 @@
 int main() {
 
     float saldo = 1000.00;
+    float emprestimo = 0.00;
     float valor;
     int opcao;
 
-    printf("1 - Saldo\n");
-    printf("2 - Deposito\n");
-    printf("3 - Saque\n");
+    do {
 
-    scanf("%d", &opcao);
+        printf("\n===== CAIXA ELETRONICO =====\n");
+        printf("1 - Verificar saldo\n");
+        printf("2 - Depositar valor\n");
+        printf("3 - Sacar valor\n");
+        printf("4 - Pedir emprestimo\n");
+        printf("5 - Pagar emprestimo\n");
+        printf("6 - Consultar emprestimo\n");
+        printf("7 - Sair\n");
+        printf("Opcao: ");
 
-    if(opcao == 1) {
+        scanf("%d", &opcao);
 
-        printf("Saldo: %.2f\n", saldo);
+        switch(opcao) {
 
-    } else if(opcao == 2) {
+            case 1:
+                printf("Saldo: %.2f\n", saldo);
+                break;
 
-        printf("Valor: ");
-        scanf("%f", &valor);
+            case 2:
+                printf("Valor deposito: ");
+                scanf("%f", &valor);
 
-        saldo += valor;
+                if(valor > 0) {
+                    saldo += valor;
+                }
+                break;
 
-        printf("Novo saldo: %.2f\n", saldo);
+            case 3:
+                printf("Valor saque: ");
+                scanf("%f", &valor);
 
-    } else if(opcao == 3) {
+                if(valor > 0 && valor <= saldo) {
+                    saldo -= valor;
+                }
+                break;
 
-        printf("Valor: ");
-        scanf("%f", &valor);
+            case 4:
+                printf("Valor emprestimo: ");
+                scanf("%f", &valor);
 
-        saldo -= valor;
+                if(valor > 0) {
+                    emprestimo += valor;
+                    saldo += valor;
+                }
+                break;
 
-        printf("Novo saldo: %.2f\n", saldo);
-    }
+            case 5:
+                printf("Valor pagamento: ");
+                scanf("%f", &valor);
+
+                if(valor > 0 &&
+                   valor <= saldo &&
+                   emprestimo > 0) {
+
+                    if(valor > emprestimo) {
+                        valor = emprestimo;
+                    }
+
+                    saldo -= valor;
+                    emprestimo -= valor;
+                }
+                break;
+
+            case 6:
+                printf("Emprestimo: %.2f\n", emprestimo);
+                break;
+
+            case 7:
+                printf("Sistema encerrado.\n");
+                break;
+
+            default:
+                printf("Opcao invalida.\n");
+        }
+
+    } while(opcao != 7);
 
     return 0;
 }
