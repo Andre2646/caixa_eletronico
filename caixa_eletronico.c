824edb68a0bfac616430fe6e59d206b7882ae3d8
Andre2646 <andre.lima25@aluno.cps.sp.gov.br>
2026-06-23 12:54:29 -0300

    Implementação de depósito e saque

--- caixa_eletronico.c
+++ caixa_eletronico.c
@@ -3,15 +3,36 @@
 int main() {
 
     float saldo = 1000.00;
+    float valor;
     int opcao;
 
-    printf("1 - Verificar saldo\n");
-    printf("7 - Sair\n");
+    printf("1 - Saldo\n");
+    printf("2 - Deposito\n");
+    printf("3 - Saque\n");
 
     scanf("%d", &opcao);
 
     if(opcao == 1) {
+
         printf("Saldo: %.2f\n", saldo);
+
+    } else if(opcao == 2) {
+
+        printf("Valor: ");
+        scanf("%f", &valor);
+
+        saldo += valor;
+
+        printf("Novo saldo: %.2f\n", saldo);
+
+    } else if(opcao == 3) {
+
+        printf("Valor: ");
+        scanf("%f", &valor);
+
+        saldo -= valor;
+
+        printf("Novo saldo: %.2f\n", saldo);
     }
 
     return 0;
